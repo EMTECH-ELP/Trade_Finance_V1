5dad82d11f13154a3451f8eb48a189ed4ddd2fb5
dennisky <56163809+Dennismugambi64@users.noreply.github.com>
2024-02-21 09:00:42 +0300

    First commit

---
+++ trade-finance-backend/src/test/java/com/emtech/tradefinancebackend/TradeFinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emtech.tradefinancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeFinanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
