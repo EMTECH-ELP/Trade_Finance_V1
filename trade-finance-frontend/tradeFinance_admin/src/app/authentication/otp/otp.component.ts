7a4be53c6641d67bff3333f9882f8bffc6910dc4
dennisky <56163809+Dennismugambi64@users.noreply.github.com>
2024-02-23 16:16:41 +0300

    admin setup

--- trade-finance-frontend/tradeFinance_admin/src/app/authentication/otp/otp.component.ts
+++ trade-finance-frontend/tradeFinance_admin/src/app/authentication/otp/otp.component.ts
@@ -120,7 +120,7 @@ export class OtpComponent implements OnInit {
       //           "Login Successful"
       //         );
 
-              this.router.navigateByUrl("/dashboard/dashboard");
+              this.router.navigateByUrl("/admin/dashboard");
             // } else {
             //   this.snackbar.showNotification("snackbar-danger", res.message);
             // }
