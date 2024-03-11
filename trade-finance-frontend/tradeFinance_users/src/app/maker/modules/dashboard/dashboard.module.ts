fc69ed9b0599a9eda058646088d0dc81148e95d9
dennisky <56163809+Dennismugambi64@users.noreply.github.com>
2024-03-11 09:34:13 +0300

    checker, maker, usage data v2

--- trade-finance-frontend/tradeFinance_users/src/app/maker/modules/dashboard/dashboard.module.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/maker/modules/dashboard/dashboard.module.ts
@@ -5,11 +5,13 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { GenWidgetsComponent } from './gen-widgets/gen-widgets.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    GenWidgetsComponent
   ],
   imports: [
     CommonModule,
