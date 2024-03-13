03c43d1bcab238b75aee618e6f926f6209f0d330
FaithAdhiambo456 <echessafaith@gmail.com>
2024-03-13 17:04:31 +0300

    component for creating a new LC

--- trade-finance-frontend/tradeFinance_users/src/app/lc/lc.module.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/lc/lc.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LcRoutingModule } from './lc-routing.module';
+import { CreateComponent } from './components/create/create.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent
+  ],
   imports: [
     CommonModule,
     LcRoutingModule
