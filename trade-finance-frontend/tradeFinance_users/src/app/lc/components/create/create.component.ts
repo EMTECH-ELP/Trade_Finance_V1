8d355c450a4b6ea7db56c78f8a5393ad231fd894
FaithAdhiambo456 <echessafaith@gmail.com>
2024-03-15 09:46:27 +0300

    Application form for creating a new LC

--- trade-finance-frontend/tradeFinance_users/src/app/lc/components/create/create.component.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/lc/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.sass']
 })
 export class CreateComponent implements OnInit {
+  selectedValue: string;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  // applicant Details
+  applicantsForm= this.fb.group ({
+    firstName: [],
+    middleName: [],
+    lastName: [],
+    phoneNumber: [],
+    businessName: [],
+    email: [],
+    accountName: [],
+    accountNumber: [],
+    address: [],
+  })
+  // beneficiary details
+  beneficiaryForm= this.fb.group({
+    firsttName: [],
+    middleName: [],
+    lastName: [],
+    country: [],
+    city: [],
+    address: [],
+    bankName: [],
+    accountName: [],
+    accountNumber: [],
+    bankCode: [],
+    swiftCode: [],
+  })
+  // LC Details
+  lcForm= this.fb.group({
+    portOfLoading: [],
+    shipmentDate: [],
+    portOfDischarge: [],
+    incoterms:[],
+    expiryDate: []
+  })
 
-  ngOnInit(): void {
+//Collateral Details
+collateralForm= this.fb.group({
+  collateralType: [],
+  collateralId: [],
+  collateralValue: [],
+  phoneNumber: []
+})
+
+//Guarantee Details
+guaranteeForm= this.fb.group({
+  fullName: [],
+  phoneNumber: [],
+  email: [],
+  address: [],
+  guaranteeAmount: [],
+})
+//Documents Upload Section
+documentsForm= this.fb.group({
+})
+
+  ngOnInit() {
+    // ngOnInit code goes here
   }
-
 }
--- trade-finance-frontend/tradeFinance_users/src/app/lc/lc.module.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/lc/lc.module.ts
@@ -3,15 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { LcRoutingModule } from './lc-routing.module';
 import { CreateComponent } from './components/create/create.component';
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    CreateComponent
+    CreateComponent,
   ],
   imports: [
     CommonModule,
-    LcRoutingModule
+    LcRoutingModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule, 
+    ReactiveFormsModule
   ]
 })
 export class LcModule { }
