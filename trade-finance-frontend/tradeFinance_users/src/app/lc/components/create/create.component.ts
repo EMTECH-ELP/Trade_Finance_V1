9e2cfe8cf1b44f1a9d52c371b36b2a5d268bce8c
FaithAdhiambo456 <echessafaith@gmail.com>
2024-03-18 16:08:43 +0300

    Update on new LC creation form

--- trade-finance-frontend/tradeFinance_users/src/app/lc/components/create/create.component.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/lc/components/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -8,62 +8,64 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class CreateComponent implements OnInit {
   selectedValue: string;
-
   constructor(private fb: FormBuilder) { }
-  // applicant Details
-  applicantsForm= this.fb.group ({
-    firstName: [],
-    middleName: [],
-    lastName: [],
-    phoneNumber: [],
-    businessName: [],
-    email: [],
-    accountName: [],
-    accountNumber: [],
-    address: [],
+  applicationForm= this.fb.group ({
+    applicantFirstName: ['', Validators.required],
+    applicantMiddleName: [''],
+    applicantLastName: ['', Validators.required],
+    applicantAddress: ['', Validators.required],
+    applicantEmail: ['',[Validators.required, Validators.email]],
+    applicantPhoneNumber: ['', Validators.required],
+    businessName: ['', Validators.required],
+    applicantAccountName: ['', Validators.required],
+    applicantAccountNumber: ['', Validators.required],
+    issuingBank: ['', Validators.required],
+    issuingSwiftCode: ['', Validators.required],
+    beneficiaryFirstName: ['', Validators.required],
+    beneficiaryMiddleName: [''],
+    beneficiaryLastName: ['', Validators.required],
+    beneficiaryAddress: ['', Validators.required],
+    beneficiaryEmail:  ['',[Validators.required, Validators.email]],
+    beneficiaryPhoneNumber: ['', Validators.required],
+    beneficiaryAccountName: ['', Validators.required],
+    beneficiaryAccountNumber: ['', Validators.required],
+    beneficiaryBank: ['', Validators.required],
+    beneficiarySwiftCode: ['', Validators.required],
+    beneficiaryCity: ['', Validators.required],
+    lcType: ['', Validators.required],
+    subType: ['', Validators.required],
+    applicableRules: ['', Validators.required],
+    isExpired: ['',Validators.required],
+    shipmentDate: ['', Validators.required],
+    portOfLoading: ['', Validators.required],
+    shipmentTerms: ['', Validators.required],
+    partialShipment: ['', Validators.required],
+    transShipment: ['', Validators.required],
+    issueDate: ['', Validators.required],
+    expiryDate: ['', Validators.required],
+    usance: ['', Validators.required],
+    transferable: ['', Validators.required],
+    negotiationPeriod: ['', Validators.required],
+    commodityCode: ['', Validators.required],
+    goodsQuantity: ['', Validators.required],
+    pricePerUnit: ['', Validators.required],
+    countyOfOrigin: ['', Validators.required],
+    chargesBorneBy: ['', Validators.required],
+    amount: ['', Validators.required],
+    amountCode: ['', Validators.required],
+    collateralType: ['', Validators.required],
+    collateralId: ['', Validators.required],
+    collateralValue: ['', Validators.required],
+    guarantorName: ['', Validators.required],
+    guarantorAddress: ['', Validators.required],
+    guarantorEmail: ['', Validators.required],
+    guarantorPhoneNumber: ['', Validators.required],
+    billOfLading: ['', Validators.required],
+    numberOfCopies: ['', Validators.required],
+    signed: ['', Validators.required],
+    documentDescription: ['', Validators.required]
   })
-  // beneficiary details
-  beneficiaryForm= this.fb.group({
-    firsttName: [],
-    middleName: [],
-    lastName: [],
-    country: [],
-    city: [],
-    address: [],
-    bankName: [],
-    accountName: [],
-    accountNumber: [],
-    bankCode: [],
-    swiftCode: [],
-  })
-  // LC Details
-  lcForm= this.fb.group({
-    portOfLoading: [],
-    shipmentDate: [],
-    portOfDischarge: [],
-    incoterms:[],
-    expiryDate: []
-  })
-
-//Collateral Details
-collateralForm= this.fb.group({
-  collateralType: [],
-  collateralId: [],
-  collateralValue: [],
-  phoneNumber: []
-})
-
-//Guarantee Details
-guaranteeForm= this.fb.group({
-  fullName: [],
-  phoneNumber: [],
-  email: [],
-  address: [],
-  guaranteeAmount: [],
-})
-//Documents Upload Section
-documentsForm= this.fb.group({
-})
+  
 
   ngOnInit() {
     // ngOnInit code goes here
