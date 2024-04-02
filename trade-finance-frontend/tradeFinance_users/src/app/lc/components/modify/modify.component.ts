f3dd704c932ddd6f9692c975796b972365eb3a4d
NicksonSaruni <159453034+NicksonSaruni@users.noreply.github.com>
2024-04-02 08:11:23 +0300

    modify checker

--- trade-finance-frontend/tradeFinance_users/src/app/lc/components/modify/modify.component.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/lc/components/modify/modify.component.ts
@@ -1,6 +1,67 @@
+// import { Component, OnInit } from '@angular/core';
+// import { LcService } from '../../services/lc.service';
+// import { FormBuilder, FormGroup } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-modify',
+//   templateUrl: './modify.component.html',
+//   styleUrls: ['./modify.component.scss']
+// })
+
+
+// export class ModifyComponent implements OnInit {
+
+
+//   searchForm: FormGroup;
+//   searchByOptions = ['ID', 'Email', 'firstname', 'Businessname', 'account no'];
+//   selectedSearchBy: string = 'ID';
+//   searchTerm: string = '';
+//   searchResults: any[] = [];
+
+//   constructor(private LcService: LcService,
+//     private formBuilder: FormBuilder) { }
+
+//   ngOnInit(): void {
+
+//     this.searchForm = this.formBuilder.group({
+
+//     });
+//   }
+
+//   search(): void {
+
+//   }
+
+//   onsubmit() { }
+
+//   onModify() { }
+
+//   Modify(result: any): void {
+//     // Implement modify functionality here
+//   }
+// }
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { LcService } from '../../services/lc.service';
+
+
+// @Component({
+//   selector: 'app-create',
+//   templateUrl: './create.component.html',
+//   styleUrls: ['./create.component.sass']
+// })
+// export class CreateComponent implements OnInit 
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { LcService } from '../../services/lc.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modify',
@@ -9,35 +70,93 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 
 
-export class ModifyComponent implements OnInit {
+export class ModifyComponent implements OnInit
 
 
-  searchForm: FormGroup;
-  searchByOptions = ['ID', 'Email', 'firstname', 'Businessname', 'account no'];
-  selectedSearchBy: string = 'ID';
-  searchTerm: string = '';
-  searchResults: any[] = [];
+{
 
-  constructor(private LcService: LcService,
-    private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  selectedValue: string;
+  applicationForm: FormGroup;
 
-    this.searchForm = this.formBuilder.group({
+  constructor(private fb: FormBuilder,
+    private lcService: LcService) { }
+ 
 
+  ngOnInit() {
+    
+    this.applicationForm = this.fb.group({
+      applicantFirstName: ['', Validators.required],
+      applicantMiddleName: [''],
+      applicantLastName: ['', Validators.required],
+      applicantAddress: ['', Validators.required],
+      applicantEmail: ['', [Validators.required, Validators.email]],
+      applicantPhoneNumber: ['', Validators.required],
+      businessName: ['', Validators.required],
+      applicantAccountName: ['', Validators.required],
+      applicantAccountNumber: ['', Validators.required],
+      issuingBank: ['', Validators.required],
+      issuingSwiftCode: ['', Validators.required],
+      beneficiaryFirstName: ['', Validators.required],
+      beneficiaryMiddleName: [''],
+      beneficiaryLastName: ['', Validators.required],
+      beneficiaryAddress: ['', Validators.required],
+      beneficiaryEmail: ['', [Validators.required, Validators.email]],
+      beneficiaryPhoneNumber: ['', Validators.required],
+      beneficiaryAccountName: ['', Validators.required],
+      beneficiaryAccountNumber: ['', Validators.required],
+      beneficiaryBank: ['', Validators.required],
+      beneficiarySwiftCode: ['', Validators.required],
+      beneficiaryCity: ['', Validators.required],
+      lcType: ['', Validators.required],
+      subType: ['', Validators.required],
+      applicableRules: ['', Validators.required],
+      isExpired: ['', Validators.required],
+      shipmentDate: ['', Validators.required],
+      portOfDischarge: ['', Validators.required],
+      portOfLoading: ['', Validators.required],
+      shipmentTerms: ['', Validators.required],
+      partialShipment: ['', Validators.required],
+      transShipment: ['', Validators.required],
+      issueDate: ['', Validators.required],
+      expiryDate: ['', Validators.required],
+      usance: ['', Validators.required],
+      transferable: ['', Validators.required],
+      negotiationPeriod: ['', Validators.required],
+      commodityCode: ['', Validators.required],
+      goodsQuantity: ['', Validators.required],
+      pricePerUnit: ['', Validators.required],
+      countyOfOrigin: ['', Validators.required],
+      chargesBorneBy: ['', Validators.required],
+      amount: ['', Validators.required],
+      amountCode: ['', Validators.required],
+      collateralType: ['', Validators.required],
+      collateralId: ['', Validators.required],
+      collateralValue: ['', Validators.required],
+      guarantorName: ['', Validators.required],
+      guarantorAddress: ['', Validators.required],
+      guarantorEmail: ['', Validators.required],
+      guarantorPhoneNumber: ['', Validators.required],
+      billOfLading: ['', Validators.required],
+      numberOfCopies: ['', Validators.required],
+      signed: ['', Validators.required],
+      documentDescription: ['', Validators.required]
     });
+  
   }
 
-  search(): void {
+  public  onSubmit() {
 
-  }
+    console.log("Form data", this.applicationForm.value);
+    this.lcService.createLc(this.applicationForm.value).subscribe({
+      next: ((response) => {
 
-  onsubmit() { }
+        console.log("Lc create response", response);
+      }),
+      error: ((err) => {
 
-  onModify() { }
-
-  Modify(result: any): void {
-    // Implement modify functionality here
+      }),
+       complete: (() => {})
+    })
   }
 }
-
