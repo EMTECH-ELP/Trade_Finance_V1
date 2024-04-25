c867a50c05118cabb993db4e4ceed2a9e2ff4733
NicksonSaruni <159453034+NicksonSaruni@users.noreply.github.com>
2024-04-25 08:29:27 +0300

    environment ts

--- trade-finance-frontend/tradeFinance_users/src/environments/environment.ts
+++ trade-finance-frontend/tradeFinance_users/src/environments/environment.ts
@@ -2,29 +2,20 @@ export const environment = {
   production: true,
 
 //LCs
-  apiUrl: 'http://192.168.137.1:8085',    //create LC
+  createApiUrl: 'http://192.168.137.1:8085',    //create LC
   getApiUrl: `http://192.137.149.240:8187`, //fetch applicant details
   getUrl: ` http://192.168.137.1:8085`,      //get all LCs details 
 getlcNoUrl: 'http://192.168.137.1: 8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber} 
-//   getApiUrl: `http://192.168.137.1:8187`, //fetch applicant details
-//   getUrl: `http://192.168.137.1:8085`,      //get all LCs details
-// getlcNoUrl: 'http://192.168.137.1:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
-// putUrl: 'http://192.168.137.1:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
+ putUrl: 'http://192.168.137.1:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
 
 
 
+ //Documentary collections
   dcApiUrl: "http://192.168.0.1:9090",
 
-// putUrl: ' http://192.168.137.1:v8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
-//   apiUrl: 'http://192.168.89.54:8085',    //create LC
-//   getApiUrl: `http://192.168.89.54:8187`, //fetch applicant details
-//   getUrl: `http://192.168.89.54:8085`,      //get all LCs details
-// getlcNoUrl: 'http://192.168.89.54:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
-// putUrl: 'http://192.168.89.54:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
 
 
 
-//   dcApiUrl: "http://192.168.0.1:9090",
 
 
 
