be65ab1704e509869415dc9dfc451b3554af778d
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2024-05-09 16:55:38 +0300

    lc post

--- trade-finance-frontend/tradeFinance_users/src/environments/environment.ts
+++ trade-finance-frontend/tradeFinance_users/src/environments/environment.ts
@@ -2,33 +2,25 @@ export const environment = {
   production: true,
 
 //LCs
-  createApiUrl: 'http://192.168.137.1:8085',    //create LC
-  getApiUrl: `http://192.168.137.1:8187`, //fetch applicant details
-  getAllUrl: `http://192.168.137.1:8085`,      //get all LCs details
-getlcNoUrl: 'http://192.168.137.1:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
-putUrl: 'http://192.168.137.1:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
-transferApiUrl: 'http://192.168.137.1:8085', //Transfer LC
+  createApiUrl: 'http://192.168.88.107:8085',    //create LC
+  getApiUrl: `http://192.168.88.107:8187`, //fetch applicant details
+  getAllUrl: `http://192.168.88.107:8085`,      //get all LCs details
+getlcNoUrl: 'http://192.168.88.107:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
+putUrl: 'http://192.168.88.107:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
+transferApiUrl: 'http://192.168.88.107:8085', //Transfer LC
+approveLcApiUrl: 'http://192.168.88.107:8085' ,// Approval of a created LC
+deleteLcApiUrl: 'http://192.168.88.107:8085',  //delete an already created LC
 
 
 
   authUrl: 'http://192.168.91.98:8082',
   resetUrl: '',
 
-  // apiUrl: 'http://192.168.137.1:8085',
+  // apiUrl: 'http://192.168.88.107:8085',
   // getApiUrl: 'http://192.168.91.98:8082',
   dcApiUrl: "http://192.168.0.1:9090",
  apiUrl: 'http://192.168.70.290:8085',
 
-// putUrl: ' http://192.168.137.1:v8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
-//   apiUrl: 'http://192.168.89.54:8085',    //create LC
-//   getApiUrl: `http://192.168.89.54:8187`, //fetch applicant details
-//   getUrl: `http://192.168.89.54:8085`,      //get all LCs details
-// getlcNoUrl: 'http://192.168.89.54:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
-// putUrl: 'http://192.168.89.54:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
-
-
-
- 
 
 //   dcApiUrl: "http://192.168.0.1:9090",
 
