e09b7d4f42c3e150b77b267a6016905a43a38a91
Mangewan <mangewan1josh@gmail.com>
2024-05-24 12:28:56 +0300

    bills

---
+++ trade-finance-frontend/tradeFinance_users/src/app/bills/components/verify-bill/verify-bill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-bill',
+  templateUrl: './verify-bill.component.html',
+  styleUrls: ['./verify-bill.component.sass']
+})
+export class VerifyBillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
