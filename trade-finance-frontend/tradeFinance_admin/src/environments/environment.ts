4d239f05ce142ec515830f2b80c413b91bebc827
nuruabdul <nuruabdul91@gmail.com>
2024-05-27 23:21:18 +0300

    Updated Bank & View Invoice forms

--- trade-finance-frontend/tradeFinance_admin/src/environments/environment.ts
+++ trade-finance-frontend/tradeFinance_admin/src/environments/environment.ts
@@ -26,7 +26,7 @@ export const environment = {
 
  //  BanksManagement
  updatebankUrl: '',
-bankUrl: 'http://192.168.89.133:8187',    //fetchbanks
+ bankUrl: 'http://192.168.89.92:8187',    //fetchbanks
 // addBankUrl: '',   //post
 
 
