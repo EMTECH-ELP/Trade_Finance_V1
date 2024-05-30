bc015f8c109dd549c8022c7072b459fe6052f942
nuruabdul <nuruabdul91@gmail.com>
2024-05-30 12:57:23 +0300

    master data

--- trade-finance-frontend/tradeFinance_admin/src/environments/environment.ts
+++ trade-finance-frontend/tradeFinance_admin/src/environments/environment.ts
@@ -33,7 +33,7 @@ export const environment = {
 
  //  BanksManagement
  updatebankUrl: '',
- bankUrl: 'http://192.168.89.92:8187',    //fetchbanks
+ bankUrl: 'http://192.168.89.245:8187',    //fetchbanks
 // addBankUrl: '',   //post
 
 };
