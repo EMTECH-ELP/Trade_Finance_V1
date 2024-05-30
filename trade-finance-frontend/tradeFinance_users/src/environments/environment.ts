f6ad7fb3b9af5b60ea1f94a9958e9b0b497b72cc
FaithAdhiambo456 <156072384+FaithAdhiambo456@users.noreply.github.com>
2024-05-30 10:43:21 +0300

    review changes

--- trade-finance-frontend/tradeFinance_users/src/environments/environment.ts
+++ trade-finance-frontend/tradeFinance_users/src/environments/environment.ts
@@ -8,16 +8,19 @@ resetUrl: '',
 
 
 //LCs
-  createApiUrl: 'http://192.168.88.246:8085',    //create LC
+  createApiUrl: 'http://192.168.88.40:8085',    //create LC
   getApiUrl: `http://192.168.91.24:8187`, //fetch applicant details
-  getAllUrl: `http://192.168.88.246:8085`,      //get all LCs details
-getlcNoUrl: 'http://192.168.88.246:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
-putUrl: 'http://192.168.88.246:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
-transferApiUrl: 'http://192.168.88.246:8085', //Transfer LC
-getUrl: ` http://192.168.88.246:8085`,      //get all LCs details
+  getAllUrl: `http://192.168.88.40:8085`,      //get all LCs details
+getlcNoUrl: 'http://192.168.88.40:8085',    //fetch Lc details by lcNumber            (enviroment.getlcNoUrl)/api/v1/LC/lcNumber/{lcNumber}
+putUrl: 'http://192.168.88.40:8085',        // Modify LC details fetched by LcNumber  (enviroment.putUrl)/api/v1/LC/lcNumber/{lcNumber}
+transferApiUrl: 'http://192.168.88.40:8085', //Transfer LC
+getUrl: ` http://192.168.88.40:8085`,      //get all LCs details
 approveLcApiUrl: 'http://192.168.56.1:8085' ,// Approval of a created LC
 deleteLcApiUrl: 'http://192.168.56.1:8085',  //delete an already created LC
 
+
+//Shipment terms
+shipmentTermsApi: 'http://192.168.89.92:8085',      //fetch all shipment terms
 
 
  //Documentary collections
@@ -44,4 +47,5 @@ billApiUrl: 'http://192.168.56.1:8085',
  deleteurl:'http://192.168.90.44:9000', 
  //bank gurantee url
 bgUrl: 'http://192.168.91.61:8085',
+
 };
