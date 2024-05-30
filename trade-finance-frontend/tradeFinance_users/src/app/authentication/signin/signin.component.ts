a558a924d3c8f8c15b93d75574bad60d73a13d9f
Mangewan <mangewan1josh@gmail.com>
2024-05-30 10:17:40 +0300

    bills

--- trade-finance-frontend/tradeFinance_users/src/app/authentication/signin/signin.component.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/authentication/signin/signin.component.ts
@@ -62,7 +62,12 @@ ngOnInit() {
       (res) => {
         console.log("Res: ", res);
    
-  
+        if (res.body.statusCode === 207) {
+          this.tokenCookieService.saveUser(res.body.entity)
+          console.log("routing to otp")
+          this.router.navigate(["/authentication/OTP"]);
+          return; // Exit the function to prevent further navigation
+        }
   
         // If reset-password is not required, handle role-based navigation for second,third etc time users
         if (res.body.entity && res.body.entity.role === "SUPER_ADMIN") {
