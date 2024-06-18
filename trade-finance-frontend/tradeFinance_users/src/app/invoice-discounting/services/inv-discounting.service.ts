4418fe4270656726f321cd958f9635edb02796db
nuruabdul <nuruabdul91@gmail.com>
2024-06-18 15:41:51 +0300

    Invoice discounting form submission & view

--- trade-finance-frontend/tradeFinance_users/src/app/invoice-discounting/services/inv-discounting.service.ts
+++ trade-finance-frontend/tradeFinance_users/src/app/invoice-discounting/services/inv-discounting.service.ts
@@ -1,5 +1,5 @@
 import { Injectable,Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError} from 'rxjs';
 import { catchError,map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
@@ -18,23 +18,43 @@ export class InvDiscountingService {
   applicationForm: any;
   selectedCountry:any;
 
-  private countrieslistUrl = 'http://192.168.88.101:8187/api/v1/Country/all'; 
-  private getCountryByNameUrl = 'http://192.168.88.101:8187/api/v1/Country/getByName';
-  private invUrl = 'http://192.168.89.247:9001';
-
+  private countrieslistUrl = 'http://192.168.88.91:8187/api/v1/Country/all'; 
+  private getCountryByNameUrl = 'http://192.168.88.91:8187/api/v1/Country/getByName';
+  // private invUrl = 'http://192.168.89.160:8085';
+  private invUrl = environment.invUrl;
   constructor(private http: HttpClient) {}
 
    // Method to post Full details
+  //  sendData(applicationForm: any) {
+  //   // Check if applicationForm is defined and is an object
+  //   if (!applicationForm || typeof applicationForm!== 'object') {
+  //     throw new Error('Invalid applicationForm data');
+  //   }
 
-   public  sendData(data: any): Observable<any> {
-    const url = `${environment.invUrl}/applicant/invoiceDetails`;
-    return this.http.post<any>(url, data);
-  }
+  //   // Convert applicationForm to JSON string
+  //   const jsonString = JSON.stringify(applicationForm);
+
+  //   // Set Content-Type header explicitly
+  //   const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  //   // Make the HTTP POST request
+  //   return this.http.post(this.invUrl, jsonString, { headers });
+  // }
+  //  public  sendData(body: any): Observable<any> {
+  //   const headers = { 'Content-Type': 'application/json' };
+  //   // const url = `${environment.invUrl}/applicant/invoiceDetails`;
+  //   return this.http.post('http://192.168.89.160:8085/applicant/invoiceDetails', body, { headers });
+  // }
   // public postApplicantDetails(data: any): Observable<any> {
   //   const url = `${environment.invUrl}/applicants/create`;
   //   return this.http.post<any>(url, data);
   // }
-  
+  sendData(items: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    return this.http.post<any>(`${environment.invUrl}/applicant/invoiceDetails`, items, { headers });
+  }
   // private handleError<T>(operation = 'operation', result?: T): (error: any, caught: Observable<T>) => Observable<T> {
   //   return (error: any, caught: Observable<T>): Observable<T> => {
   //     console.error(`${operation}: ${error.message}`); // Log the error
@@ -51,10 +71,7 @@ export class InvDiscountingService {
   //   return this.http.post<any>(url, data);
   // }
   
-  public postData(data: any): Observable<any> {      //Posting repayment details
-    const url = `${environment.saveUrl}/repayment`;
-    return this.http.post<any>(url, data);
-  }
+
 
   // Method to fetch all forms
 
@@ -64,7 +81,7 @@ export class InvDiscountingService {
     
   }
  // public getAllForms(): Observable<any> {
-  //   const url = `${this.invUrl}/invoices/list`;
+  //   const url = `${this.invUrl}/applicant/Invoice`;
   //   return this.http.get<any>(url).pipe(
   //     catchError(this.handleError('getAllForms', []))
   //   );
@@ -77,9 +94,11 @@ export class InvDiscountingService {
 // }
 
  //View invoice by Id
-  getData(url:string):Observable<any>{
-      return this.http.get(url)              
-  }
+ getDataById(id: number): Observable<any> {
+  console.log('Data received:', id);
+  return this.http.get<any>(`${environment.invUrl}/applicant/${id}`);
+}
+
 
   getCountries(): Observable<string[]> {
     return this.http.get<string[]>(this.countrieslistUrl).pipe(
@@ -96,7 +115,27 @@ export class InvDiscountingService {
     return this.http.get<any[]>(`${this.getCountryByNameUrl}/${countryName}`);
   }
   
+  getDataBasedOnDiscountRate(discountRate: number): Observable<any> {
+    // Replace 'your-backend-api-url' with your actual backend API endpoint
+    const url = `your-backend-api-url?discountRate=${discountRate}`;
 
+    return this.http.get<any>(url)
+      .pipe(
+        catchError((error: any) => {
+          console.error('Error fetching data:', error);
+          throw error; // Rethrow the error or handle as needed
+        })
+      );
+  }
+
+  saveFundingData(){
+
+  }
+
+  public postPaymentData(data: any): Observable<any> {      //Posting repayment details
+    const url = `${environment.saveUrl}/repayment`;
+    return this.http.post<any>(url, data);
+  }
 }
 
 
